c13ba76dc75bc605cecfd92ac7e33096a9a70f23
Vikas Choudhry <vchoud1@L-SB835Q4G8W-M.homeoffice.wal-mart.com>
2015-11-04 15:14:34 -0800

    initial commit

---
+++ app/index.jsx
@@ -0,0 +1,16 @@
+//import './main.css';
+
+import React from 'react';
+import App from './components/App.jsx';
+
+main();
+
+function main() {
+  const app = document.createElement('div');
+
+  document.body.appendChild(app);
+
+  React.render(<App />, app);
+}
+
+
